4f49524cbf558c531294e2a40ed66186c2e2e3df
Eric Grant <senatorericgrant@gmail.com>
2020-06-01 01:37:45 +0700

    feat: add MainTabBarController

---
+++ MySImpleMusicPlayer/MainTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBarController.swift
+//  MySImpleMusicPlayer
+//
+//  Created by Eric Grant on 01.06.2020.
+//  Copyright © 2020 Eric Grant. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+    }
+}
