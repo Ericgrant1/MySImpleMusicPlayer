282e94dbe8b8336cd5c59498aaa04223ea98f89c
Eric Grant <senatorericgrant@gmail.com>
2020-06-01 16:02:28 +0700

    feat: implement main Tab Bar Controller

--- MySImpleMusicPlayer/MainTabBarController.swift
+++ MySImpleMusicPlayer/MainTabBarController.swift
@@ -14,16 +14,23 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .gray
+        view.backgroundColor = .white
         
-        let searchVC = SearchViewController()
-        let navigationVC = UINavigationController(rootViewController: searchVC)
-        
-        let libraryVC = ViewController()
+        tabBar.tintColor = #colorLiteral(red: 1, green: 0, blue: 0.3764705882, alpha: 1)
         
         viewControllers = [
-            navigationVC,
-            libraryVC
+            generateViewController(rooviewController: SearchViewController(), image: #imageLiteral(resourceName: "ios10-apple-music-search-5nav-icon"), title: "Search"),
+            generateViewController(rooviewController: ViewController(), image: #imageLiteral(resourceName: "ios10-apple-music-library-5nav-icon"), title: "Library")
         ]
+    }
+    
+    func generateViewController(rooviewController: UIViewController, image: UIImage, title: String) -> UIViewController {
+        let navigationVC = UINavigationController(rootViewController: rooviewController)
+        navigationVC.tabBarItem.image = image
+        navigationVC.tabBarItem.title = title
+        rooviewController.navigationItem.title = title
+        navigationVC.navigationBar.prefersLargeTitles = true
+        
+        return navigationVC
     }
 }
--- MySImpleMusicPlayer/SearchViewController.swift
+++ MySImpleMusicPlayer/SearchViewController.swift
@@ -14,6 +14,6 @@ class SearchViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
     }
 }
