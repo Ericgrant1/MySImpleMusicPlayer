7bfe386d5952d3cde7ba686ff94302a7e6efa3c1
Eric Grant <senatorericgrant@gmail.com>
2020-06-03 15:46:26 +0700

    feat: create Data Model

---
+++ MySImpleMusicPlayer/SearchResponse.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResponse.swift
+//  MySImpleMusicPlayer
+//
+//  Created by Eric Grant on 03.06.2020.
+//  Copyright © 2020 Eric Grant. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResponse: Decodable {
+    var resultCount: Int
+    var results: [Track]
+}
+struct Track: Decodable {
+    var trackName: String
+    var collectionName: String
+    var artistName: String
+    var artworkUrl100: String?
+}
