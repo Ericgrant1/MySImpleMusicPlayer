89dc509f034ff7689190f661ad1bcd4086ec6a2c
Eric Grant <senatorericgrant@gmail.com>
2020-06-05 22:41:29 +0700

    feat: implement Player of Tracks

--- MySImpleMusicPlayer/SearchVC/TrackDetailView/TrackDetailView.swift
+++ MySImpleMusicPlayer/SearchVC/TrackDetailView/TrackDetailView.swift
@@ -9,10 +9,17 @@
 import Foundation
 import UIKit
 import SDWebImage
+import AVKit
 
 class TrackDetailView: UIView {
     
     // MARK: - Properties
+    
+    let player: AVPlayer = {
+        let avPlayer = AVPlayer()
+        avPlayer.automaticallyWaitsToMinimizeStalling = false
+        return avPlayer
+    }()
     
     @IBOutlet weak var trackImageView: UIImageView!
     @IBOutlet weak var currentTimeSlider: UISlider!
@@ -31,6 +38,8 @@ class TrackDetailView: UIView {
         trackImageView.backgroundColor = .red
     }
     
+    // MARK: - IBActions
+    
     @IBAction func dragDownButtonTapped(_ sender: Any) {
         
         self.removeFromSuperview()
@@ -44,6 +53,13 @@ class TrackDetailView: UIView {
     @IBAction func nextTrack(_ sender: Any) {
     }
     @IBAction func playPauseAction(_ sender: Any) {
+        if player.timeControlStatus == .paused {
+            player.play()
+            playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+        } else {
+            player.pause()
+            playPauseButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+        }
     }
     
     // MARK: - Helper functions
@@ -51,9 +67,19 @@ class TrackDetailView: UIView {
     func set(viewModel: SearchViewModel.Cell) {
         trackTitleLabel.text = viewModel.trackName
         authorTitleLabel.text = viewModel.artistName
+        playTrack(previewUrl: viewModel.previewUrl)
         
         let string600 = viewModel.iconUrlString?.replacingOccurrences(of: "100x100", with: "600x600")
         guard let url = URL(string: string600 ?? "") else { return }
         trackImageView.sd_setImage(with: url, completed: nil)
+    }
+    
+    private func playTrack(previewUrl: String?) {
+        print("Trying to turn on a track by link \(previewUrl ?? "not available")")
+        
+        guard let url = URL(string: previewUrl ?? "") else { return }
+        let playerItem = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
     }
 }
