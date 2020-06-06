e4551fe30f47f401cd17e24093bc24452bfb6044
Eric Grant <senatorericgrant@gmail.com>
2020-06-06 15:40:44 +0700

    feat: create scale animations of Track

--- MySImpleMusicPlayer/SearchVC/TrackDetailView/TrackDetailView.swift
+++ MySImpleMusicPlayer/SearchVC/TrackDetailView/TrackDetailView.swift
@@ -35,6 +35,9 @@ class TrackDetailView: UIView {
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        let scale: CGFloat = 0.8
+        trackImageView.transform = CGAffineTransform(scaleX: scale, y: scale)
+        trackImageView.layer.cornerRadius = 5
         trackImageView.backgroundColor = .red
     }
     
@@ -53,12 +56,15 @@ class TrackDetailView: UIView {
     @IBAction func nextTrack(_ sender: Any) {
     }
     @IBAction func playPauseAction(_ sender: Any) {
+        
         if player.timeControlStatus == .paused {
             player.play()
             playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+            enlargeTrackImageView()
         } else {
             player.pause()
             playPauseButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+            reduceTrackImageView()
         }
     }
     
@@ -81,5 +87,30 @@ class TrackDetailView: UIView {
         let playerItem = AVPlayerItem(url: url)
         player.replaceCurrentItem(with: playerItem)
         player.play()
+    }
+    
+    // MARK: - Animations
+    
+    private func enlargeTrackImageView() {
+        UIView.animate(withDuration: 1,
+                       delay: 0,
+                       usingSpringWithDamping: 0.5,
+                       initialSpringVelocity: 1,
+                       options: .curveEaseOut,
+                       animations: {
+            self.trackImageView.transform = .identity
+        }, completion: nil)
+    }
+    
+    private func reduceTrackImageView() {
+        UIView.animate(withDuration: 1,
+                       delay: 0,
+                       usingSpringWithDamping: 0.5,
+                       initialSpringVelocity: 1,
+                       options: .curveEaseOut,
+                       animations: {
+            let scale: CGFloat = 0.8
+            self.trackImageView.transform = CGAffineTransform(scaleX: scale, y: scale)
+        }, completion: nil)
     }
 }
